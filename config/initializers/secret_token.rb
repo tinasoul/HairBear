ea0f39cde26ea6c37a45812c6770d316d1b55e4b
tinasoul <tinapshah@gmail.com>
2013-08-03 11:57:31 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HairBear::Application.config.secret_token = '318780d84272cc7347f60f0cd87f0b561182a96bbb0d416a40d4c45216f9a7fee120fed229f211fcd1eb9b3af0a52b60d8d9f8883c45f70e0222379f681a0b4b'
